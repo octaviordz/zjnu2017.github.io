59dc35dba8ac1ddb80293e0a56379d535b149f6e
bkenwright <b.kenwright@napier.ac.uk>
2017-09-18 02:51:40 +0800

    Second lecture.

---
+++ OOP/examples/test2.java
@@ -0,0 +1,24 @@
+
+// 1 - Compile:
+// >javac test.javac
+
+// 2 - Execute
+// >java -cp . test
+
+// 3 - Output 
+// 'test Java'
+
+// Note: '-cp .' add the current directory to the class path
+
+
+
+class test2
+{ 
+	public static void main(String[] args) 
+	{ 
+		int x = 1; if (x == 1) { x = x + 1; } 
+		
+		System.out.println("x: " + x);
+	} 
+}
+
